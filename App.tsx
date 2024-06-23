e378de3feb1f1d4c4700610321ea684da404a7dc
Arturo Valdes <36093379+pollovaldes@users.noreply.github.com>
2024-06-23 01:21:19 -0600

    Grouped list

--- App.tsx
+++ App.tsx
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Platform, SafeAreaView, ScrollView, StyleSheet } from "react-native";
+import GroupedList from "./components/GroupedList";
+import Row from "./components/Row";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollview}>
+        <GroupedList
+          header="Datos"
+          footer="Usa estos menús para conocer más sobre los datos de este camión en particular."
+        >
+          <Row
+            title="Documentación"
+            trailingType="chevron"
+            caption="Ver más"
+            icon="home-filled"
+            color="#2427de"
+          />
+          <Row title="Ficha técnica" trailingType="chevron" />
+          <Row title="Histórico de cargas" trailingType="chevron" />
+          <Row title="Histórico de rutas" trailingType="chevron" />
+        </GroupedList>
+
+        <GroupedList header="Acciones">
+          <Row title="Registrar carga de combustible" trailingType="chevron" />
+          <Row title="Solicitar mantenimiento" trailingType="chevron" />
+        </GroupedList>
+        <StatusBar style="auto" />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+  },
+  scrollview: {
+    backgroundColor: "#f2f2f7",
+    width: "100%", // Ensures full width on mobile screens
+    ...Platform.select({
+      web: {
+        maxWidth: 400, // Restricts to max 400 points on web
+        minWidth: 250,
+      },
+    }),
   },
 });
